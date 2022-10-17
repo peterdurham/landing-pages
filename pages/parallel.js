cec8f36f4cfa6ee8984395d9ed45b8a9494c69f3
Peter Durham <peterdurham@protonmail.com>
2022-10-17 10:27:11 -0700

    added header section

--- pages/parallel.js
+++ pages/parallel.js
@@ -12,6 +12,11 @@ const ParallelWrapper = styled.div`
   a {
     font-family: 'Work Sans', sans-serif;
   }
+  h1 {
+    font-family: 'Michroma', sans-serif;
+    margin: 0;
+    padding: 0;
+  }
 
   .michroma {
     font-family: 'Michroma', sans-serif;
@@ -27,7 +32,38 @@ const Parallel = () => {
       <Navbar />
       <h1>Default</h1>
       <Button label="Sign up Free"></Button>
+      <Header />
     </ParallelWrapper>
+  )
+}
+
+const HeaderWrapper = styled.div`
+  margin: 0 60px;
+  h1 {
+    font-size: 150px;
+    line-height: 170px;
+  }
+  p {
+    text-align: center;
+  }
+  .text-left {
+    text-align: left;
+  }
+  .text-right {
+    text-align: right;
+  }
+`
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <h1 className="text-left">Land a job</h1>
+      <h1 className="text-right">the easy way</h1>
+      <p>
+        The best place to discover & apply to the coolest start up jobs, without
+        the black box.
+      </p>
+    </HeaderWrapper>
   )
 }
 
